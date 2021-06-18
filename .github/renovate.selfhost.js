8e2304726cf3e48fd9ff49efde847e901e4fd935
Timtor Chen <to@timtor.dev>
2021-06-19 01:31:40 +0800

    action - update renovate to v25

--- .github/renovate.selfhost.js
+++ .github/renovate.selfhost.js
@@ -5,7 +5,6 @@ module.exports = {
   platform: "github",
   repositories: ["timtorChen/pi-cluster"], 
   gitAuthor: "Renovate Bot <renovate@whitesourcesoftware.com>",
-  logLevel: "debug",
   onboarding: true,
   onboardingBranch: "action/config",
   onboardingConfigFileName: "renovate.json5",
