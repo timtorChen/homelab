81c70ae955f878ac98fcee703b146b0495907c74
Timtor Chen <to@timtor.dev>
2024-11-25 15:09:28 +0800

    chore: remove unused terraform/local

--- terraform/local/data.tf
+++
@@ -1,9 +0,0 @@
-# Parameter store secrets
-data "aws_ssm_parameter" "ceph-admin" {
-  name = "/amethyst/ceph-admin"
-}
-
-locals {
-  ceph_s3_access_key = jsondecode(data.aws_ssm_parameter.ceph-admin.value)["access_key"]
-  ceph_s3_secret_key = jsondecode(data.aws_ssm_parameter.ceph-admin.value)["secret_key"]
-}
