753d42f4bc0ca5b47a971ad6fedfdf033cbc5f61
Timtor Chen <to@timtor.dev>
2025-06-01 20:09:15 +0800

    feat: add argo-workflows

--- terraform/aws/kubernetes-irsa.tf
+++ terraform/aws/kubernetes-irsa.tf
@@ -1203,3 +1203,46 @@ resource "aws_iam_role_policy_attachment" "miniflux-postgres-secret-holder" {
   role       = aws_iam_role.miniflux-postgres-secret-holder.name
   policy_arn = aws_iam_policy.miniflux-postgres-secret-holder.arn
 }
+
+resource "aws_iam_role" "argo-workflows" {
+  name = "${local.project}-argo-workflows"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "${aws_iam_openid_connect_provider.kubernetes-oidc.arn}"
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "${aws_iam_openid_connect_provider.kubernetes-oidc.url}:sub" : "system:serviceaccount:argo-workflows:argo-workflows-server",
+            "${aws_iam_openid_connect_provider.kubernetes-oidc.url}:aud" : "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "argo-workflows" {
+  name = "${local.project}-argo-workflows"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "ssm:GetParameters",
+        "Effect" : "Allow",
+        "Resource" : [
+          "arn:aws:ssm:${data.aws_region.main.name}:${data.aws_caller_identity.main.account_id}:parameter/kubernetes/argo-workflows"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "argo-workflows" {
+  role       = aws_iam_role.argo-workflows.name
+  policy_arn = aws_iam_policy.argo-workflows.arn
+}
