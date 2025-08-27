bd3c10577eb159c4fba437c8932a7dc5c0480dd6
Timtor Chen <to@timtor.dev>
2025-08-28 03:32:25 +0800

    keep

--- terraform/mikrotik/main.tf
+++ terraform/mikrotik/main.tf
@@ -2,23 +2,24 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.97.0"
+      version = "~> 6.10.0"
     }
     routeros = {
       source = "terraform-routeros/routeros"
-      version = "~> 1.85.3"
+      version = "~> 1.86.3"
     }
   }
   backend "s3" {
     bucket         = "amethyst-terraform-backend"
     key            = "homelab/mikrotik"
-    dynamodb_table = "tfstate-lock"
+    use_lockfile   = true
     region         = "us-west-2"
   }
 }
 
 provider "routeros" {
-  hosturl = "api://192.168.248.1:8080"
+  hosturl = "http://192.168.248.1:8080"
   username = local.mikrotik_username
-  password = local.mikrotik_passsword
+  password = local.mikrotik_password
 }
+
