d388013ab4080a9947f5f64f300d377fb252881a
Timtor Chen <to@timtor.dev>
2025-11-14 17:36:11 +0800

    implement cilium bgp - add mikrotik terraform

---
+++ terraform/mikrotik/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.10.0"
+    }
+    routeros = {
+      source = "terraform-routeros/routeros"
+      version = "~> 1.86.3"
+    }
+  }
+  backend "s3" {
+    bucket         = "amethyst-terraform-backend"
+    key            = "homelab/mikrotik"
+    use_lockfile   = true
+    region         = "us-west-2"
+  }
+}
+
+provider "routeros" {
+  hosturl = "http://192.168.248.1:8080"
+  username = local.mikrotik_username
+  password = local.mikrotik_password
+}
+
+locals {
+  homelab_private_domains = [
+    "ui.timtor.dev",
+    "hass.timtor.dev",
+    "grafana.timtor.dev",
+    "drive.timtor.dev",
+    "rss.timtor.dev"
+  ]
+}
